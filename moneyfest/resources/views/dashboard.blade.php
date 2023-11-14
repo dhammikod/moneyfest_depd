0667646805898065e1e502630de195044ecf8ada
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-11-14 21:44:34 +0700

    input ui (almost?)

--- moneyfest/resources/views/dashboard.blade.php
+++ moneyfest/resources/views/dashboard.blade.php
@@ -117,96 +117,104 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Income</title>
-  {{-- <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" /> --}}
-  <link rel="stylesheet" href="assets/css/dashboard.css" />
-  <link rel="stylesheet" href="assets/css/dashboard2.css" />
-  <link rel="stylesheet" href="assets/css/icon/icon.css">
-  <link rel="stylesheet" href="assets/css/icon/tablericon.svg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Income</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css" />
+    <link rel="stylesheet" href="assets/css/dashboard2.css" />
+    <link rel="stylesheet" href="assets/css/icon/icon.css">
+    <link rel="stylesheet" href="assets/css/icon/tablericon.svg">
+    <!-- Include DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Include DataTables JS -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 </head>
 
 <body>
-  <!--  Body Wrapper -->
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-    data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-    <aside class="left-sidebar">
-      <!-- Sidebar scroll-->
-      <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="./dashboard" class="text-nowrap logo-img">
-            <img src="assets/images/logo/logo-dark.png" width="180" alt="" />
-          </a>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-            <i class="ti ti-x fs-8"></i>
-          </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            {{-- <li class="nav-small-cap">
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div>
+                <div class="brand-logo d-flex align-items-center justify-content-between">
+                    <a href="./dashboard" class="text-nowrap logo-img">
+                        <img src="assets/images/logo/logo-dark.png" width="180" alt="" />
+                    </a>
+                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                        <i class="ti ti-x fs-8"></i>
+                    </div>
+                </div>
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+                    <ul id="sidebarnav">
+                        {{-- <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">Home</span>
             </li> --}}
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./xxxxx.php" aria-expanded="false">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Dashboard</span>
-              </a>
-            </li>
-            {{-- <li class="nav-small-cap">
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./xxxxx.php" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-layout-dashboard"></i>
+                                </span>
+                                <span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
+                        {{-- <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">UI COMPONENTS</span>
             </li> --}}
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./dashboard" aria-expanded="false">
-                <span>
-                  <i class="ti ti-article"></i>
-                </span>
-                <span class="hide-menu">Income</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./pengeluaran" aria-expanded="false">
-                <span>
-                  <i class="ti ti-alert-circle"></i>
-                </span>
-                <span class="hide-menu">Expense</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-cards"></i>
-                </span>
-                <span class="hide-menu">Salary</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-file-description"></i>
-                </span>
-                <span class="hide-menu">Profit</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
-                <span>
-                  <i class="ti ti-typography"></i>
-                </span>
-                <span class="hide-menu">Stock</span>
-              </a>
-            </li>
-            {{-- <li class="nav-small-cap">
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./dashboard" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-article"></i>
+                                </span>
+                                <span class="hide-menu">Income</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./pengeluaran" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-alert-circle"></i>
+                                </span>
+                                <span class="hide-menu">Expense</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-cards"></i>
+                                </span>
+                                <span class="hide-menu">Salary</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-file-description"></i>
+                                </span>
+                                <span class="hide-menu">Profit</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-typography"></i>
+                                </span>
+                                <span class="hide-menu">Stock</span>
+                            </a>
+                        </li>
+                        {{-- <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">AUTH</span>
             </li> --}}
-            {{-- <li class="sidebar-item">
+                        {{-- <li class="sidebar-item">
               <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
                 <span>
                   <i class="ti ti-login"></i>
@@ -214,7 +222,7 @@
                 <span class="hide-menu">Subscription</span>
               </a>
             </li> --}}
-            {{-- <li class="sidebar-item">
+                        {{-- <li class="sidebar-item">
               <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
                 <span>
                   <i class="ti ti-user-plus"></i>
@@ -242,30 +250,31 @@
                 <span class="hide-menu">Sample Page</span>
               </a>
             </li> --}}
-          </ul>
-          <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
-            <div class="d-flex">
-              <div class="unlimited-access-title me-3">
-                <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
-                <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/" target="_blank" class="btn btn-primary fs-2 fw-semibold lh-sm">Subscribe Now</a>
-              </div>
-              <div class="unlimited-access-img">
-                <img src="assets/images/background/rocket.png" alt="" class="img-fluid">
-              </div>
+                    </ul>
+                    <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
+                        <div class="d-flex">
+                            <div class="unlimited-access-title me-3">
+                                <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
+                                <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/"
+                                    target="_blank" class="btn btn-primary fs-2 fw-semibold lh-sm">Subscribe Now</a>
+                            </div>
+                            <div class="unlimited-access-img">
+                                <img src="assets/images/background/rocket.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <!-- End Sidebar navigation -->
             </div>
-          </div>
-        </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
-    <!--  Sidebar End -->
-    <!--  Main wrapper -->
-    <div class="body-wrapper">
-      <!--  Header Start -->
-      <header class="app-header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          {{-- <ul class="navbar-nav">
+            <!-- End Sidebar scroll-->
+        </aside>
+        <!--  Sidebar End -->
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            <!--  Header Start -->
+            <header class="app-header">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    {{-- <ul class="navbar-nav">
             <li class="nav-item d-block d-xl-none">
               <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
                 <i class="ti ti-menu-2"></i>
@@ -278,10 +287,18 @@
               </a>
             </li>
           </ul> --}}
-          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-              <a href="https://adminmart.com/product/modernize-free-bootstrap-admin-dashboard/" target="_blank" class="btn btn-primary">Logout</a>
-              {{-- <li class="nav-item dropdown">
+                    <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+                        <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+                            {{-- <a href="https://adminmart.com/product/modernize-free-bootstrap-admin-dashboard/"
+                                target="_blank" class="btn btn-primary">Logout</a> --}}
+                                <div>
+                                    <form action="/logout" method="post">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary"
+                                            style="font-size: 12px;  background: #2D36A1; color: white; padding: 8px 12px; border: none; border-radius: 4px; cursor: pointer;">Logout</button>
+                                    </form>
+                                </div>
+                            {{-- <li class="nav-item dropdown">
                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
                   aria-expanded="false">
                   <img src="assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
@@ -304,12 +321,12 @@
                   </div>
                 </div>
               </li> --}}
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <!--  Header End -->
-      {{-- <div class="container-fluid">
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <!--  Header End -->
+            {{-- <div class="container-fluid">
         <!--  Row 1 -->
         <div class="row">
           <div class="col-lg-8 d-flex align-items-strech">
@@ -659,24 +676,56 @@
         </div>
       </div> --}}
 
-      <div class="container-fluid">
-        <div class="card w-100 h-100 position-relative overflow-hidden">
-          <div class="card-body">
-            <h5 class="card-title fw-semibold mb-4">Icons</h5>
+            <div class="container-fluid">
+                <div class="card w-100 h-100 position-relative overflow-hidden">
+                    <div class="card-body" style="background-color: #CEDCEE">
+                        {{-- <h5 class="card-title fw-semibold mb-4">Icons</h5>
             <iframe src="https://tabler-icons.io/" frameborder="0" style="height: calc(100vh - 250px); width: 100%;"
-              data-simplebar=""></iframe>
-          </div>
+              data-simplebar=""></iframe> --}}
+                        <div style="display: flex; justify-content: flex-end">
+                            <form method="post" action="" class="form" style="text-align: center">
+                                <h2 style="font-weight: bold">Input New Transaction</h2>
+                                @csrf
+                                <input type="hidden" name="jenis" value="pendapatan"">
+
+                                <select id="kategori" name="kategori"
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    <!-- Options will be dynamically populated using JavaScript -->
+                                </select>
+
+                                <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                <input type="text" name="name" id="name" placeholder="Name of Income"
+                                    required style="border-color: #2F80ED; background-color: #E0E9F4">
+                                <input type="number" name="nominal" id="nominal" placeholder="Nominal" required
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                <input type="number" name="jumlah" id="jumlah" placeholder="Amount" required
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                <input type="text" name="satuan" id="satuan" placeholder="Unit" required
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                <input type="text" name="tanggal" id="tanggal" placeholder="Date" required
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                <input type="text" name="catatan" id="catatan" placeholder="Notes" required
+                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+
+                                <button type="submit"
+                                    style="background: white; padding: 8px 12px; border: none; border-radius: 4px; cursor: pointer; font-weight: bold">SAVE</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/js/sidebarmenu.js"></script>
-  <script src="../assets/js/app.min.js"></script>
-  <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="../assets/js/dashboard.js"></script>
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/sidebarmenu.js"></script>
+    <script src="../assets/js/app.min.js"></script>
+    <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+    <script src="../assets/js/dashboard.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script src="assets/js/autoload_form.js"></script>
 </body>
 
 </html>
