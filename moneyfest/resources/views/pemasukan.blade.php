cb75871d51296787ada54e06ee1cea0df308703b
vebrillia s <vsantoso01@ciputra.ac.id>
2023-11-20 00:01:44 +0700

    connecting stock page to another page

--- moneyfest/resources/views/pemasukan.blade.php
+++ moneyfest/resources/views/pemasukan.blade.php
@@ -88,7 +88,7 @@
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+                            <a class="sidebar-link" href="/stok" aria-expanded="false">
                                 <span>
                                     <i class="ti ti-typography"></i>
                                 </span>
