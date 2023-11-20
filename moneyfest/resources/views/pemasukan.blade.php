e8ec38298fb6b68835d30aa2dac4fff47acf7ccd
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-11-20 20:52:49 +0700

    fixing pemasukan

--- moneyfest/resources/views/pemasukan.blade.php
+++ moneyfest/resources/views/pemasukan.blade.php
@@ -18,6 +18,15 @@
 
     <!-- Include DataTables JS -->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+
+    <style>
+        @media (min-width: 768px) {
+            .center-content {
+                display: flex;
+                justify-content: space-evenly;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -127,94 +136,125 @@
                             </a>
                         </li>
                     </ul>
-                        <div class="navbar-collapse justify-content-start px-0" id="navbarNav">
-                            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        <img src="../assets/images/testimonials/avatar-1.png" alt=""
-                                            width="35" height="35" class="rounded-circle">
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
-                                        aria-labelledby="drop2">
-                                        <div class="message-body">
-                                            <a href="javascript:void(0)"
-                                                class="d-flex align-items-center gap-2 dropdown-item">
-                                                <i class="ti ti-user fs-6"></i>
-                                                <p class="mb-0 fs-3">My Profile</p>
-                                            </a>
-                                            <a href="javascript:void(0)"
-                                                class="d-flex align-items-center gap-2 dropdown-item">
-                                                <i class="ti ti-mail fs-6"></i>
-                                                <p class="mb-0 fs-3">My Account</p>
-                                            </a>
-                                            <form action="/logout" method="post">
-                                                @csrf
-                                                <button type="submit" class="btn btn-outline-primary mx-3 mt-2 d-block">
-                                                    <a>Logout</a>
-                                                </button>
-                                            </form>
-                                        </div>
+                    <div class="navbar-collapse justify-content-start px-0" id="navbarNav">
+                        <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../assets/images/testimonials/avatar-1.png" alt=""
+                                        width="35" height="35" class="rounded-circle">
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                                    aria-labelledby="drop2">
+                                    <div class="message-body">
+                                        <a href="javascript:void(0)"
+                                            class="d-flex align-items-center gap-2 dropdown-item">
+                                            <i class="ti ti-user fs-6"></i>
+                                            <p class="mb-0 fs-3">My Profile</p>
+                                        </a>
+                                        <a href="javascript:void(0)"
+                                            class="d-flex align-items-center gap-2 dropdown-item">
+                                            <i class="ti ti-mail fs-6"></i>
+                                            <p class="mb-0 fs-3">My Account</p>
+                                        </a>
+                                        <form action="/logout" method="post">
+                                            @csrf
+                                            <button type="submit" class="btn btn-outline-primary mx-3 mt-2 d-block">
+                                                <a>Logout</a>
+                                            </button>
+                                        </form>
                                     </div>
-                                </li>
-                                <div>
-
                                 </div>
-                            </ul>
-                        </div>
+                            </li>
+                            <div>
+
+                            </div>
+                        </ul>
+                    </div>
                 </nav>
             </header>
             <!--  Header End -->
 
+
             <div class="container-fluid">
                 <div class="card w-100 h-100 position-relative overflow-hidden">
-                    <div class="card-body" style="background-color: #CEDCEE">
-                        <div style="display: flex; justify-content: flex-start">
-                            <div class="col-md-4">
-                                <h5 class="card-title fw-semibold mb-4">Income Patterns</h5>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <table>
-                                            @foreach ($kategories_sum as $item)
-                                                <tr>
-                                                    <td>
-                                                        <h5 class="card-title">{{ $item['kategori'] }}</h5>
-                                                    </td>
-                                                    <td>
-                                                        <h6 class="card-subtitle mb-2 text-muted">
-                                                            {{ $item['total_category'] }}</h6>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </table>
+                    <div class="card-body center-content flex-wrap"
+                        style="background-color: #CEDCEE; display: flex; flex-wrap: wrap">
+                        <div class="col-lg-6 col-md-12 mb-3">
+                            <h5 class="card-title mb-4"><b>Income Patterns</b></h5>
+
+                            {{-- @foreach ($kategories_sum as $item)
+                                <div class="card mb-3">
+                                    <div class="card-body d-flex justify-content-between align-items-center">
+                                        <h5 class="card-title mb-0">{{ $item['kategori'] }}</h5>
+                                        <h6 class="card-subtitle text-muted">{{ $item['total_category'] }}</h6>
                                     </div>
                                 </div>
-                            </div>
+                            @endforeach --}}
+
+                            @foreach ($kategories_sum as $item)
+                                <a href="/histori/{histori}">
+                                    <div class="card mb-3">
+                                        <div class="card-body d-flex justify-content-between align-items-center">
+                                            <h5 class="card-title mb-0">{{ $item['kategori'] }}</h5>
+                                            <h6 class="card-subtitle text-muted">{{ $item['total_category'] }}</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            @endforeach
+
+
+                            {{-- @foreach ($kategories_sum as $item)
+                                <div class="card mb-3">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="card-body d-flex justify-content-between align-items-center">
+                                                <h5 class="card-title mb-0">{{ $item['kategori'] }}</h5>
+                                                <h6 class="card-subtitle text-muted">{{ $item['total_category'] }}</h6>
+                                            </div>
+                                        </div>
+                                        <div class="progress mt-3">
+                                            <div class="progress-bar" role="progressbar"
+                                                style="width: {{ $item['percentage'] }}%;"
+                                                aria-valuenow="{{ $item['percentage'] }}" aria-valuemin="0"
+                                                aria-valuemax="100">{{ $item['percentage'] }}%</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach --}}
+
                         </div>
-                        <div style="display: flex; justify-content: flex-end">
+                        <div style="col-lg-6 col-md-12">
                             <form method="post" action="" class="form" style="text-align: center">
                                 <h2 style="font-weight: bold; padding: 20px">Input New Income</h2>
                                 @csrf
                                 <input type="hidden" name="jenis" value="pendapatan">
 
                                 <select id="kategori" name="kategori"
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                     <!-- Options will be dynamically populated using JavaScript -->
                                 </select>
 
                                 <input type="hidden" name="user_id" value="{{ $user->id }}">
                                 <input type="text" name="name" id="name" placeholder="Name of Income"
-                                    required style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    required style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                 <input type="number" name="nominal" id="nominal" placeholder="Nominal" required
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                 <input type="number" name="jumlah" id="jumlah" placeholder="Amount" required
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                 <input type="text" name="satuan" id="satuan" placeholder="Unit" required
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                 <input type="text" name="tanggal" id="tanggal" placeholder="Date" required
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
                                 <input type="text" name="catatan" id="catatan" placeholder="Notes" required
-                                    style="border-color: #2F80ED; background-color: #E0E9F4">
+                                    style="border-color: #2F80ED; background-color: #E0E9F4"
+                                    class="form-control mb-3">
 
                                 <button type="submit"
                                     style="background: white; padding: 8px 12px; border: none; border-radius: 4px; cursor: pointer; font-weight: bold">SAVE</button>
@@ -223,6 +263,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
     <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
