af03287208a875809eeba063b6f4f40dc1d4031b
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-12-22 13:04:45 +0700

    fixing title

--- moneyfest/resources/views/keuntungan.blade.php
+++ moneyfest/resources/views/keuntungan.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CapcAI Dashboard</title>
+    <title>Profit</title>
     <link rel="shortcut icon" type="image/png" href="../assets/images/logo/.png" />
     <link rel="stylesheet" href="assets/css/dashboard.css" />
     <link rel="stylesheet" href="../assets/css/dashboard2.css" />
--- moneyfest/resources/views/stok.blade.php
+++ moneyfest/resources/views/stok.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Stock</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <!-- Include DataTables CSS -->
